d655e580b5bd1280ce82f255579e7591f05c2790
naitoyuma3230 <naitoyuma@outlook.jp>
2022-12-01 07:00:05 +0900

    fire

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+	faUser,
+	faSignOutAlt,
+	faEllipsisV,
+} from "@fortawesome/free-solid-svg-icons";
 
-Vue.config.productionTip = false
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import firebase from "firebase";
+import { initializeApp } from "firebase/app"; // firebase/appモジュールをインポート
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite"; // firestoreをインポート
+Vue.config.productionTip = false;
+Vue.component("fa", FontAwesomeIcon);
+library.add(faUser, faSignOutAlt, faEllipsisV);
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAVbaMi0wv2PwszFHx0I8BNitiYXHydjyo",
+	authDomain: "vue-whisper.firebaseapp.com",
+	projectId: "vue-whisper",
+	storageBucket: "vue-whisper.appspot.com",
+	messagingSenderId: "161235616759",
+	appId: "1:161235616759:web:9b040367f7f7554650a4f2",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+	router,
+	render: function (h) {
+		return h(App);
+	},
+}).$mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    HELLO
+    <div class="list">
+      <li v-for="whisper in whispers" :key="whisper.id" class="item">
+        {{whisper.content}}
+        {{whisper.uid}}
+      </li>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {  db } from '../main' 
 
 export default {
-  name: 'HomeView',
+  name: 'home',
   components: {
-    HelloWorld
+  }, 
+  data () {
+    return {
+      whispers: []
+    }
+  },
+firestore () {
+  return {
+    whispers: db.collection('whispers')
   }
 }
+}
 </script>
+
